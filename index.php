a7f6443000975f55f6b7a4f52b4f9836f1853e04
Robin <robinrid6@gmail.com>
2024-11-20 12:56:46 +0100

    updated some css and added first view

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php $title = 'Home'; require_once 'assets/sidebar.php'; ?>
+<?php $title = 'Home'; 
+$css = 'plant-overview.css';
+require_once 'assets/sidebar.php'; ?>
+
+<section>
+    <div class="plant">
+        <div class="plant-img">
+            <img src="assets/images/plant.jpg" alt="plant">
+        </div>
+        <div class="plant-info">
+            <p>Plant:<br>Fungi Nuisance</p>
+            <p>Aantal water gekregen:<br>21</p>
+            <p>Vochtigheid percentage:<br>100%</p>
+            <p>Tijd van uitgave<br>11:49</p>
+        </div>
+        <div class="plant-warning">
+            <i class="fas fa-exclamation-triangle"></i>
+        </div>
+    </div>
+</section>
 </body>
+
 </html>
