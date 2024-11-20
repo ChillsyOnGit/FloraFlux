29aa091826acbb471d72e8a7b9860d6220a47b4b
Robin <robinrid6@gmail.com>
2024-11-20 14:55:58 +0100

    easy way to show more plants

--- index.php
+++ index.php
@@ -3,125 +3,28 @@ $css = 'plant-overview.css';
 require_once 'assets/sidebar.php'; ?>
 
 <section>
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        echo '<a href="plant.php?id='.$i.'">
+        <div class="plant">
+            <div class="plant-img">
+                <img src="assets/images/plant.jpg" alt="plant">
+            </div>
+            <div class="plant-info">
+                <p>Plant:<br>Fungi Nuisance</p>
+                <p>Aantal water gekregen:<br>21</p>
+                <p>Vochtigheid percentage:<br>100%</p>
+                <p>Tijd van uitgave<br>11:49</p>
+            </div>
+            <div class="plant-warning">
+                <i class="fas fa-exclamation-triangle"></i>
+            </div>
         </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
+        </a>';
+    }
+    ?>
 
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
-
-    <div class="plant">
-        <div class="plant-img">
-            <img src="assets/images/plant.jpg" alt="plant">
-        </div>
-        <div class="plant-info">
-            <p>Plant:<br>Fungi Nuisance</p>
-            <p>Aantal water gekregen:<br>21</p>
-            <p>Vochtigheid percentage:<br>100%</p>
-            <p>Tijd van uitgave<br>11:49</p>
-        </div>
-        <div class="plant-warning">
-            <i class="fas fa-exclamation-triangle"></i>
-        </div>
-    </div>
+    
 </section>
 </body>
 
