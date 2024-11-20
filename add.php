a925e20e31ca27c66910e6aae2b45b05c2f1d779
Mike van den Berg <24098043@student.hhs.nl>
2024-11-20 14:44:34 +0100

    Plant add scherm

--- add.php
+++ add.php
@@ -1,11 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php $title = 'Plant toevoegen'; 
+$css = 'plant-add.css';
+require_once 'assets/sidebar.php'; ?>
+
+<section>
     
+    <div class="box1">
+        <h1>Nieuwe plant</h1>
+        <br><br><br>
+     <form action ="login.php" method="post">
+        <input type="text" name="Naam plant" placeholder="Naam plant">
+        <br>
+        <input type="text" name="Pomp ID" placeholder="Pomp ID">
+        <br>
+        <select id="Plant soort">
+            <option selected disabled>Soort plant</option>
+            <option value="Dolle-Kervel">Dolle Kervel</option>
+            <option value="Verlariaan">Valeriaan</option>
+            <option value="Gele-Lis">Gele Lis</option>
+            <option value="Zombo">Zombo</option>
+        </select>
+        <br>
+        <input class="toevoegen" type="submit" value="Toevoegen">
+    </div>
+</section>
 </body>
+
 </html>
