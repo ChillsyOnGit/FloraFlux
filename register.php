af41a598c589ea771dc1f570cd48019dcf753b06
JDanielKM <danielgamer0501@gmail.com>
2024-11-27 14:31:50 +0100

    reg

--- register.php
+++ register.php
@@ -10,18 +10,19 @@
 <body>
     
     <div class="box2">
-        <h2>inloggen</h2>
+        <h2>Aanmelden</h2>
      <form action ="login.php" method="post">
         <input type="email" name="email" placeholder="E-mail" required>
-        <input type="text" name="vnaam" placeholder="Voornaam" required>
-        <input type="text" name="anaam" placeholder="Achternaam">
+        <div><input type="text" name="vnaam" placeholder="Voornaam" required>
+        <input type="text" name="anaam" placeholder="Achternaam"></div>
         <input type="tel" name="tnummer" placeholder="Telefoonnummer">
-        <input type="password" name="password" placeholder="Wachtwoord" required>
-        <input type="password" name="bpassword" placeholder="Bevestig Wachtwoord" required>
+        <div><input type="password" name="password" placeholder="Wachtwoord" required>
+        <input type="password" name="bpassword" placeholder="Bevestig Wachtwoord" required></div>
         <input type="submit" value="Inloggen">
     </form>
-    <p>Nog geen account?
-    Meld je <a href='register.php'>hier</a> aan</p>
+    <div class="text"><p>*verplicht veld</php>
+    <p>Al een account?
+    Log je <a href='register.php'>hier</a> in</p></div>
 </div>   
 </body>
 </html>
